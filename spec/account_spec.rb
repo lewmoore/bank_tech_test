31aa24865ae6d8b60b59e89f7fccc44db0276003
Lewis Moore <lewis.moore2707@gmail.com>
2018-02-26 14:26:06 +0000

    Add passing test for account deposit function

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -9,4 +9,11 @@ describe Account do
       expect(account.balance).to eq 0
     end
   end
+
+  describe '#deposit' do
+    it 'A desposit should increase the account balance' do
+      account.deposit(8000)
+      expect(account.balance).to eq 8000
+    end
+  end
 end
